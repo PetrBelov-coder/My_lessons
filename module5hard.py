4dede736d8f95498c17bbd2e5d55bc13507872f3
PetrBelov-coder <BelovPA@yandex.ru>
2024-08-12 18:13:03 +0300

    Для консультации с преподавателем
    Написал метод регистрации и залогинивания. Хотел запустить, чтобы оттестировать. Создал экземпляр класса. Прога ругается -что-то с синтаксисом обращения к экземпляру класса.

---
+++ module5hard.py
@@ -0,0 +1,92 @@
+# Задание "Свой YouTube"
+# Всего будет 3 класса: User, Video, UrTube.
+#  https://github.com/MihailMelikyan/modul-5.Hard/blob/master/main.py
+
+class User :
+    """
+    Атрибуты:
+    nickname(имя пользователя, строка),
+    password(в хэшированном виде, число),
+    age(возраст, число)
+    """
+    def __init__(self, nickname, password, age):
+        self.nickname = str(nickname)
+        self.password = hash(int(password))
+        self.age = int(age)
+
+class Video :
+    """
+    Атрибуты:
+    title(заголовок, строка),
+    duration(продолжительность, секунды),
+    time_now(секунда остановки (изначально 0)),
+    adult_mode(ограничение по возрасту,
+    bool (False по умолчанию)
+    """
+    def __init__(self, title, duration, time_now, adult_mode, bool):
+        self.title = str(title)
+        self.duration = int(duration)
+        self.time_now = 0
+        self.adult_mode = int(adult_mode)
+        self.bool = False
+
+class UrTube :
+    """
+    Атриубты:
+    users(список объектов User),
+    videos(список объектов Video),
+    current_user(текущий пользователь, User)
+
+    Методы:
+    register,   который принимает три аргумента: nickname, password, age, и
+                добавляет пользователя в список, если пользователя не существует (с таким же nickname).
+                если существует, выводит на экран: "Пользователь {nickname} уже существует".
+                После регистрации, вход выполняется автоматически.
+    log_in,     который принимает на вход аргументы: nickname, password и пытается найти пользователя в users
+    log_out     для сброса текущего пользователя на None.
+    add,        который принимает неограниченное кол-во объектов класса Video и все добавляет в videos,
+                если с таким же названием видео ещё не существует.
+                В противном случае ничего не происходит.
+    get_videos, который принимает поисковое слово и возвращает список названий всех видео, содержащих поисковое слово.
+                Регистр не учитывает.
+    watch_video, который принимает название фильма, если не находит точного совпадения(вплоть до пробела),
+                то ничего не воспроизводится,
+                если же находит - ведётся отчёт в консоль на какой секунде ведётся просмотр.
+                После текущее время просмотра данного видео сбрасывается.
+    """
+    def __init__(self, users, videos,current_user):
+        self.users = []
+        self.videos = []
+        self.current_user = None
+    def register(self, nickname, password, age):
+        self.nickname = str(input("Введите Ваш логин : ", ))
+        self.password = int(input("Введите Ваш пароль : ", ))
+        self.age = int(input("Введите Ваш возраст : ", ))
+        for self.nickname in self.users :
+            if User.nickname == self.nickname :
+                print("Пользователь ", self.nickname, " уже зарегистрирован ")
+            else :
+                self.current_user = [self.nickname, self.password , self.age ]
+                self.users.append(self.current_user)
+        print(self.nickname, ", Вы зарегистрированы.  Добро пожаловать в UrTube! ")
+
+    def log_in(self,nickname, password ):
+        if self.current_user is None :
+            self.nickname = input("Введите Ваш логин : " )
+            self.password = int(input("Введите Ваш пароль : " ))
+            for user in self.users :
+                if user.nickname == self.nickname and user.password  == hash(self.password) :
+                    self.current_user = user
+                    print(self.nickname, "Добро пожаловать в UrTube! ")
+                    break
+ur1 = UrTube()
+ur1.register("Myself", 12345, 10)
+
+
+
+
+
+
+
+
+
