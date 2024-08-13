ae2dcb6b1a3ef27bb2fba6f377e4db66f02c9b8a
PetrBelov-coder <BelovPA@yandex.ru>
2024-08-13 23:24:56 +0300

    Для консультации с Егором. Строки 129, 131, и далее под комментами вопросы, на которые я не могу ответить. Прошу помощи.

--- module5hard.py
+++ module5hard.py
@@ -54,48 +54,85 @@ class UrTube :
                 если же находит - ведётся отчёт в консоль на какой секунде ведётся просмотр.
                 После текущее время просмотра данного видео сбрасывается.
     """
-    def __init__(self, users, videos, current_user = None):
-        self.users = users
-        self.videos = videos
+
+    def __init__(self, current_user = None):
+        self.users = []
+        self.videos = []
         self.current_user = current_user
+
     def register(self, nickname, password, age):
+        user = User(nickname, hash(password), age)
+        self.current_user = user
+        self.users.append(user)
+        print(user.nickname, ", Вы зарегистрированы в БД UrTube. Ваша учетная запись :  ",
+            user.nickname, user.password, user.age)
+    def log_in(self, nickname, password):
         self.nickname = nickname
-        self.password = hash(password)
-        self.age = age
-        for user in self.users :
-            if user.nickname == self.nickname :
-                print("Пользователь ", self.nickname, " уже зарегистрирован ")
-                break
-            else :
-                user = User(self.nickname, hash(self.password), self.age)
-                self.current_user = user
-                self.users.append(user)
-        print(self.nickname, ", Вы зарегистрированы в БД UrTube. Ваша учетная запись :  ", self.current_user )
-        print("Список всех пользователей БД UrTube : ", users)
-        # print(self.nickname, ", Добро пожаловать в UrTube! ")
-    def log_in(self,nickname, password ):
-        if self.current_user is None :
-            self.nickname = input("Введите Ваш логин : " )
-            self.password = int(input("Введите Ваш пароль : " ))
-            for user in self.users :
-                if user.nickname == self.nickname and user.password == hash(self.password) :
+        self.password = int(password)
+        if self.current_user is None:
+            for user in self.users:
+                if user.nickname == self.nickname and user.password == hash(self.password):
                     self.current_user = user
                     print(self.nickname, "Добро пожаловать в UrTube! ")
                     break
-users = []
-videos = []
-user = User("Я", 1, 10)
-print(user.nickname, user.password, user.age )
-ur1 = UrTube(users, videos)
-ur1.register("Me", 1, 70)
+                else :
+                    print("К сожалению, Вы отсутствуете в БД. Пожалуйста, зарегистрируйтесь")
+            else :
+                print("Вы уже зарегистрировались, ", self.nickname )
 
+    def log_out(self, current_user) :
+        self.current_user = current_user
+        print("До следующей встречи, ", self.current_user.nickname)
+        self.current_user = None
 
+    def add(self, videos, *all_videos):
+            # Откуда берётся неопределенный список *all_videos,
+            # который надо сравнивать с имеющимся списком videos?
+        self.videos = videos
+        for i in all_videos:
+            if i not in self.videos:
+                self.videos.append(i)
 
+    def get_videos(self, your_str):
+        self.your_str = your_str.lower()
+        list_video = []
+        for i in self.videos:
+            if self.your_str in i.title.lower() :
+                list_video.append(i.title)
+                flag = True
+            else:
+                flag = False
+        if flag :
+            print("Ключевое слово ", your_str , " найдено в следующих видео: ")
+            for i in list_vid :
+                print(i.title)
+        else :
+            print("Названия видео в videos не содержат ключевого слова ", your_str)
 
+    def watch_video(self, wanted_video):
+        self.wanted_video = wanted_video
+        for i in self.videos:
+            if self.wanted_video == i.title:
+                if self.current_user.age < i.adult_mode:
+                    print(" Это видео имеет ограничение по возрасту: ", i.adult_mode )
+                    break
+                for j in range(0, i.duration):
+                    print("Просмотр видео идет ", j , " секунд. До конца видео осталось ", i.duration - j, " секунд")
 
-
-
-
-
-
-
+ur1 = UrTube()
+ur1.register("Me", 1, 70 )
+ur1.register("You", 2, 72)
+print("Список всех пользователей БД UrTube : ")
+for i in ur1.users :
+    print(i.nickname, i.password, i.age)
+ur1.log_in("He", 3 )
+        # he не зарегистрирован, но залогинился
+ur1.log_in("Me", 3)
+        # Me залогинился под паролем he
+print("Список всех пользователей БД UrTube : ")
+        # список users не изменился после залогинивания He и Me под чужим паролем
+for i in ur1.users:
+    print(i.nickname, i.password, i.age)
+    # He и Me, под чужим паролем, не зарегистрированы новыми пользователями
+#    ur1.log_out("Me", 3)
+    # TypeError: UrTube.UrTube.log_out() takes 2 positional arguments but 3 were given
